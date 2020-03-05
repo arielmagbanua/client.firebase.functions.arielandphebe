657cbdb75267ed0e444a27db1494b39a53493d61
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-05 20:54:23 +0800

    Added helper script for emulating firebase functions locally.

--- functions-serve.bat
+++ functions-serve.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 set PROJECT_PATH=%cd%
-set GOOGLE_APPLICATION_CREDENTIALS=%PROJECT_PATH%\gappcreds.json
+set GOOGLE_APPLICATION_CREDENTIALS=%PROJECT_PATH%\arielandphebe-functions-ai.json
 echo "Verifying Access Token..."
 call gcloud auth application-default print-access-token
 echo "Starting Emulators..."
--- functions-serve.sh
+++ functions-serve.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export GOOGLE_APPLICATION_CREDENTIALS="$PWD/gappcreds.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$PWD/arielandphebe-functions-ai.json"
 echo "Verifying Access Token..."
 gcloud auth application-default print-access-token
 echo "Starting Emulators..."
